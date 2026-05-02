370bea61649777b5e84fa58d310803dadc881f9a
Prerana Sinha <152646017+preranasinha20@users.noreply.github.com>
2026-05-02 11:52:54 +0530

    Create DBConnection.java

---
+++ JDBC/DBConnection.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+public class DBConnection {
+    static final String URL = "jdbc:mysql://localhost:3306/RestaurantDB";
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
